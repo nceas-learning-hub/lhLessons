8034f4c7438303c91f9b4e7b22bd255a9812605c
Casey O'Hara <cohara@ucsb.edu>
2024-10-28 16:39:13 -0700

    feat: updating styling to better match NCEAS
    Added an Avenir-like font, changed header colors in css
    
    Added a title-block-banner to the _quarto.yml with color from NCEAS page

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Example coreR course
+#'
+#' A subset of lessons ordered according to coreR October 2024 course
+#'
+#' @format ## `example_course`
+#' A data frame with 10 rows and 2 columns:
+#' \describe{
+#'   \item{module}{Module name}
+#'   \item{course}{The name of the course file}
+#' }
+#'
+"example_course"
